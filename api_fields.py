3adbf27099052b6c0574ef76bcb4c46aef54fc99
marketing-zo-td <marketingvl.zo.td@gmail.com>
2024-06-03 15:01:29 +1000

    Update api_fields.py

--- api_fields.py
+++ api_fields.py
@@ -1,10 +1,10 @@
-hit_field_list = [
-    "ym:pv:regionCountry"
-]
-
 visit_field_list = [
     "ym:s:counterUserIDHash",
     "ym:s:lastTrafficSource",
     "ym:s:purchaseID",
     "ym:s:purchaseRevenue"
+]
+
+hit_field_list = [
+    "ym:pv:regionCountry"
 ]
