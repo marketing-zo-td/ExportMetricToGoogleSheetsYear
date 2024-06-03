fff0ac33620eeaaa0e9f5f31a87faf3e76fb29f4
marketing-zo-td <marketingvl.zo.td@gmail.com>
2024-06-03 13:01:09 +1000

    Update api_fields.py

--- api_fields.py
+++ api_fields.py
@@ -1,11 +1,10 @@
 hit_field_list = [
     "ym:pv:date",
-    "ym:pv:lastTrafficSource"
 ]
 
 visit_field_list = [
     "ym:s:date",
-    "ym:s:deviceCategory",
+    "ym:s:visitID",
     "ym:s:lastTrafficSource",
     "ym:s:purchaseID",
     "ym:s:purchaseRevenue"
